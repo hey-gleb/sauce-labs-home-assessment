6f2da48df8a3eacad7d4995489aa3a503e5045ed
Gleb Kheilovskii <mr.heylovskiy@gmail.com>
2024-08-30 11:24:51 +0200

    Added tailwind support. Added tailwind styles

--- fizzbuzzbass-frontend/src/App.tsx
+++ fizzbuzzbass-frontend/src/App.tsx
@@ -4,8 +4,6 @@ import Input from "./components/input/Input";
 
 import { submitGameValue } from "./api/submitGameValue";
 
-import "./App.css";
-
 interface ErrorConfig {
   visible: boolean;
   message?: string;
@@ -55,25 +53,49 @@ const App: React.FC = () => {
   );
 
   return (
-    <div className="App">
-      <h1>Fizz Buzz Bass game</h1>
-      <h3>Enter some number value to proceed</h3>
-      <form onSubmit={onFormSubmit}>
-        <Input
-          placeholder={"Some number value"}
-          ref={inputRef}
-          onFocus={eraseInputError}
-          errorConfig={inputErrorConfig}
-        />
-        <button type={"submit"}>Submit</button>
-      </form>
-      {result && (
-        <>
-          <h3>Result</h3>
-          <p>{result}</p>
-        </>
-      )}
-    </div>
+    <section className="px-4 px- mx-auto max-w-lg max-w-l justify-center m-10">
+      <div className="p-5 flex border border-blue-200 flex-col rounded-2xl shadow-2xl w-[450px] h-[280px]">
+        <div className={"text-center"}>
+          <h1 className={"text-2xl font-bold text-gray-900 mb-1"}>
+            Fizz Buzz Bass game
+          </h1>
+          <h3 className={"text-m font-medium text-gray-500"}>
+            Enter some number value to proceed
+          </h3>
+        </div>
+        <form
+          className={"h-full rounded px-8 pt-6 pb-8"}
+          onSubmit={onFormSubmit}
+        >
+          <div
+            className={
+              "mb-[25px] flex border-b border-blue-500 py-2 justify-between items-start"
+            }
+          >
+            <Input
+              className={"w-full mr-[30px]"}
+              placeholder={"Some number value"}
+              ref={inputRef}
+              onFocus={eraseInputError}
+              errorConfig={inputErrorConfig}
+            />
+            <button
+              className={
+                "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              }
+              type={"submit"}
+            >
+              Submit
+            </button>
+          </div>
+          {result && (
+            <div className={"text-center text-2xl font-medium text-gray-900"}>
+              {result}
+            </div>
+          )}
+        </form>
+      </div>
+    </section>
   );
 };
 
--- fizzbuzzbass-frontend/src/components/input/Input.tsx
+++ fizzbuzzbass-frontend/src/components/input/Input.tsx
@@ -18,10 +18,27 @@ const Input = React.forwardRef(
       ...otherProps
     } = props;
 
+    const errorInputStyles = errorConfig.visible
+      ? "border-red-600 text-red-500 bg-red-100"
+      : null;
+
     return (
-      <div className={className}>
-        <input ref={ref} {...otherProps} />
-        {errorConfig.visible && <p>{errorConfig?.message}</p>}
+      <div className={`h-[70px] ${className}`}>
+        <input
+          aria-invalid={errorConfig.visible}
+          className={
+            "block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400" +
+            "focus:outline-none focus:ring-1 focus:border-blue-500 " +
+            errorInputStyles
+          }
+          ref={ref}
+          {...otherProps}
+        />
+        {errorConfig.visible && (
+          <p className={"text-s mt-[5px] text-red-500"}>
+            {errorConfig?.message}
+          </p>
+        )}
       </div>
     );
   },
