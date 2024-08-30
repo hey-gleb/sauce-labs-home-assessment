d30f8ca61e8f21d189cd17298ae8e506b1e0f55f
Gleb Kheilovskii <mr.heylovskiy@gmail.com>
2024-08-30 17:19:35 +0200

    Added CORS middleware for BE

--- fizzbuzzbass-backend/app/main.py
+++ fizzbuzzbass-backend/app/main.py
@@ -1,12 +1,23 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.loader import api_router
 
 app = FastAPI(
     title="FizzBuzzBass backend",
     description="Home task for developing a FizzBuzzBass application",
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
